16f0680f44dab602ebddeb92da17d0f48703272d
Martin Winkel <martin@pythomation.de>
2025-10-12 20:51:55 +0200

    refactor: simplify rest screen

--- lib/screens/rest.dart
+++ lib/screens/rest.dart
@@ -11,44 +11,40 @@ class RestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    var workoutState = context.watch<WorkoutState>();
 
-    // TODO: can i do this without Consumer? the RestScreen is initialised via ChnangeNotifierProvider after all...
-    return Consumer<WorkoutState>(
-      builder: (context, workoutState, child) {
-        if (!workoutState.isResting()) {
-          WidgetsBinding.instance.addPostFrameCallback(
-            (_) => Navigator.pop(context),
-          );
-        }
+    if (!workoutState.isResting()) {
+      WidgetsBinding.instance.addPostFrameCallback(
+        (_) => Navigator.pop(context),
+      );
+    }
 
-        return Scaffold(
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('😴', style: theme.textTheme.displayMedium),
-                  const SizedBox(height: 16),
-                  Text(
-                    formatMinutesSeconds(workoutState.restTimeRemaining),
-                    style: theme.textTheme.displayMedium,
-                  ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      workoutState.resume();
-                      // no need to call Navigator.pop(...) here as the state change
-                      // causes this function to rerun and call the same code that
-                      // would run if the timer reached 0.
-                    },
-                    child: Text('Skip'),
-                  ),
-                ],
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('😴', style: theme.textTheme.displayMedium),
+              const SizedBox(height: 16),
+              Text(
+                formatMinutesSeconds(workoutState.restTimeRemaining),
+                style: theme.textTheme.displayMedium,
               ),
-            ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  workoutState.resume();
+                  // no need to call Navigator.pop(...) here as the state change
+                  // causes this function to rerun and call the same code that
+                  // would run if the timer reached 0.
+                },
+                child: Text('Skip'),
+              ),
+            ],
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
