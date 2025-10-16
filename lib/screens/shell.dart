85519b07df91e2b8feeb8519a93ec0ca52cd69b3
Martin Winkel <martin@pythomation.de>
2025-10-16 22:48:21 +0200

    feat: bottom navigation bar

---
+++ lib/screens/shell.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pull_up_ritual/screens/selection.dart';
+import 'package:pull_up_ritual/states/tab.dart';
+
+class Shell extends StatelessWidget {
+  const Shell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tab = context.watch<TabState>();
+
+    return Scaffold(
+      body: IndexedStack(
+        index: tab.index,
+        children: [WorkoutSelectionScreen(), Placeholder()],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: tab.index,
+        onDestinationSelected: tab.setIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.fitness_center_outlined),
+            selectedIcon: Icon(Icons.fitness_center),
+            label: 'Selection',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.history_outlined),
+            selectedIcon: Icon(Icons.history),
+            label: 'History',
+          ),
+        ],
+      ),
+    );
+  }
+}
