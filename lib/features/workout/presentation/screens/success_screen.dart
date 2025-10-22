25c16301620a3c2bf59654fdb6036ee8d04dee7c
Martin Winkel <martin@pythomation.de>
2025-10-23 00:36:29 +0200

    feat: consistently apply background gradient to all screens

--- lib/features/workout/presentation/screens/success_screen.dart
+++ lib/features/workout/presentation/screens/success_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pull_up_ritual/core/widgets/gradient_scaffold.dart';
 import 'package:pull_up_ritual/shared/widgets/home_button.dart' show HomeButton;
 import 'package:pull_up_ritual/features/workout/presentation/widgets/set_cards.dart'
     show SetCards;
@@ -18,40 +19,38 @@ class SuccessScreen extends StatelessWidget {
     final durationText = formatMinutesSeconds(workout.durationSeconds() ?? 0);
     final totalReps = workout.totalReps();
 
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text(
-                '🎉\nWorkout\nCompleted!',
-                style: theme.textTheme.displayMedium,
-                textAlign: TextAlign.center,
-              ),
-              Column(
-                children: [
-                  Text(
-                    workout.workoutType.name,
-                    style: theme.textTheme.headlineMedium,
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    "Duration: $durationText",
-                    style: theme.textTheme.headlineSmall,
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    "Total Reps: $totalReps",
-                    style: theme.textTheme.headlineSmall,
-                  ),
-                  const SizedBox(height: 40),
-                  SetCards(values: getSetCardValues(workout)),
-                ],
-              ),
-              HomeButton(text: "Home"),
-            ],
-          ),
+    return GradientScaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              '🎉\nWorkout\nCompleted!',
+              style: theme.textTheme.displayMedium,
+              textAlign: TextAlign.center,
+            ),
+            Column(
+              children: [
+                Text(
+                  workout.workoutType.name,
+                  style: theme.textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  "Duration: $durationText",
+                  style: theme.textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  "Total Reps: $totalReps",
+                  style: theme.textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 40),
+                SetCards(values: getSetCardValues(workout)),
+              ],
+            ),
+            HomeButton(text: "Home"),
+          ],
         ),
       ),
     );
