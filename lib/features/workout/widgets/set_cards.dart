6d5278b56577688dcc94e516f3c3ee5f3e289c1f
Martin Winkel <martin@pythomation.de>
2025-10-27 20:01:23 +0100

    feat: dirty hack to center the setcard entries if there are <5

--- lib/features/workout/widgets/set_cards.dart
+++ lib/features/workout/widgets/set_cards.dart
@@ -9,12 +9,35 @@ class SetCards extends StatelessWidget {
   final List<String> values;
   final int numExpectedCards;
   const SetCards({super.key, required this.values, int? numExpectedCards})
-    : numExpectedCards = numExpectedCards ?? values.length;
+    : assert(
+        numExpectedCards == null || numExpectedCards >= values.length,
+        'numExpectedCards must be >= values.length',
+      ),
+      numExpectedCards = numExpectedCards ?? values.length;
 
   static const int _columnCount = 5;
 
   @override
   Widget build(BuildContext context) {
+    var children = List.generate(
+      numExpectedCards,
+      (i) => i < values.length
+          ? _SetCard(value: values[i])
+          : const _SetCard.placeholder(),
+    );
+
+    if (children.length < _columnCount && children.length % 2 == 1) {
+      // this is a dirty hack to visually center the entries in the grid
+      // i like using GridView.count as I don't have to manually define the
+      // SetCard dimensions. But I didn't find a convenient way to center
+      // the cards when there are fewer entries than columns.
+      final int numPlaceholders = (_columnCount - numExpectedCards) ~/ 2;
+      children.insertAll(
+        0,
+        List.generate(numPlaceholders, (i) => const _SetCard.hidden()),
+      );
+    }
+
     return Container(
       width: double.infinity,
       decoration: BoxDecoration(
@@ -29,25 +52,34 @@ class SetCards extends StatelessWidget {
         mainAxisSpacing: AppSpacing.md,
         crossAxisSpacing: AppSpacing.sm,
         childAspectRatio: 4 / 3,
-        children: List.generate(
-          numExpectedCards,
-          (i) => i < values.length ? _SetCard(value: values[i]) : _SetCard(),
-        ),
+        children: children,
       ),
     );
   }
 }
 
 class _SetCard extends StatelessWidget {
-  const _SetCard({this.value = placeholderValue});
+  const _SetCard({required this.value});
+  const _SetCard.placeholder() : value = _placeholderValue;
+  const _SetCard.hidden() : value = _hiddenValue;
 
-  static const String placeholderValue = "?";
+  static const String _hiddenValue = "";
+  static const String _placeholderValue = "?";
   final String value;
 
   @override
   Widget build(BuildContext context) {
+    final double opacity;
+    switch (value) {
+      case _hiddenValue:
+        opacity = 0.0;
+      case _placeholderValue:
+        opacity = 0.3;
+      default:
+        opacity = 1.0;
+    }
     return Opacity(
-      opacity: value == placeholderValue ? 0.3 : 1.0,
+      opacity: opacity,
       child: GradientSurface(
         gradient: AppGradients.secondary,
         borderRadius: BorderRadius.circular(AppSpacing.radiusMedium),
